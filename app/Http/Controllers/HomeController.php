dcf15d35aa9e2c72a4b52f3ef45eb9854ef4c2cc
Rake Wangsa <rakekw28@gmail.com>
2023-05-07 23:58:58 +0700

    commit 44

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('user.home', [
+            'title' => 'Dats Center - Home',
+            'active' => 'home',
+        ]);
+    }
 }
