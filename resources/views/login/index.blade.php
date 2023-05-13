21ba3a81997587ee4925b3add63a9444f60935f5
Rake Wangsa <rakekw28@gmail.com>
2023-05-13 08:23:50 +0700

    commit 11

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,6 +1,6 @@
 @include('layouts.head')
 
-<main>
+<main style="background-image: url('/admintemplate/img/bintang.gif'); background-repeat: no-repeat; background-size: cover;">
    <div class="container">
       <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
       @if (session()->has('success'))
@@ -19,17 +19,17 @@
       <div class="container">
          <div class="row justify-content-center">
             <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
-               <div class="card mb-3">
-                  <div class="card-body login">
+               <div class="card mb-3" style="background-color: rgba(106, 0, 255, 0);">
+                  <div class="card-body login border border-white" style="background-color: rgba(37, 37, 37, 0.5);">
                      <div class="pt-4 pb-2">
-                        <h5 class="card-title text-center pb-0 fs-4">LOGIN</h5>
+                        <h5 class="card-title text-center pb-0 fs-4 text-light">LOGIN</h5>
                      </div>
                      <form action="/login" method="post">
                         @csrf
                         <div class="form-floating">
-                           <input type="email" name="email" class="form-control
-                              @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" required value="{{ old('email') }}">
-                                 <label for="email">Email address</label>
+                           <input type="email" name="email" class="form-control text-light
+                              @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" style="background-color: rgba(129, 129, 129, 0.2);" required value="{{ old('email') }}">
+                                 <label for="email" class="text-light">Email address</label>
                               @error('email')
                               <div class="invalid-feedback">
                                  {{ $message }}
@@ -37,13 +37,13 @@
                               @enderror
                         </div>
              
-                        <div class="form-floating">
-                           <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
-                           <label for="password">Password</label>
+                        <div class="form-floating mt-2">
+                           <input type="password" name="password" class="form-control text-light" id="password" style="background-color: rgba(129, 129, 129, 0.2);" placeholder="Password" required>
+                           <label for="password" class="text-light">Password</label>
                         </div>
                                  
-                        <div class="col-12 mt-4 mb-2"> <button class="btn btn-primary w-100" type="submit">Login</button></div>
-                        <p class="small">Belum Punya Akun? <a href="/register">Register</a></p>
+                        <div class="col-12 mt-4 mb-2"> <button class="btn w-100 text-light" style="background-color: rgba(52, 0, 94, 1);" type="submit">Login</button></div>
+                        <p class="small text-light">Belum Punya Akun? <a href="/register">Register</a></p>
                      </form>
                   </div>
                </div>
